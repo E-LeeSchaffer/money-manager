5175623d6083df3ed0fb5a31f10431d4725bc0c5
Eun-Rae Lee-Schaffer <eunraelee.erl@gmail.com>
2024-10-05 11:55:34 +0200

    add styling to radio buttons

--- components/TransactionForm.js
+++ components/TransactionForm.js
@@ -1,8 +1,12 @@
 import { categories } from "@/lib/categories";
+import { useState } from "react";
 import CurrencyInput from "react-currency-input-field";
+import styled from "styled-components";
 
 export default function TransactionForm({ onAdd }) {
   const today = new Date().toISOString().split("T")[0];
+
+  const [selectedType, setSelectedType] = useState("income");
 
   function handleSubmit(event) {
     event.preventDefault();
@@ -63,12 +67,26 @@ export default function TransactionForm({ onAdd }) {
           </select>
 
           <label htmlFor="type">Type</label>
-          <div id="type">
-            <input id="income" name="type" type="radio" value="income" />
+          <StyledToggleButton id="type">
+            <input
+              id="income"
+              name="type"
+              type="radio"
+              value="income"
+              checked={selectedType === "income"}
+              onChange={() => setSelectedType("income")}
+            />
             <label htmlFor="income">Income</label>
-            <input id="expense" name="type" type="radio" value="expense" />
+            <input
+              id="expense"
+              name="type"
+              type="radio"
+              value="expense"
+              checked={selectedType === "expense"}
+              onChange={() => setSelectedType("expense")}
+            />
             <label htmlFor="expense">Expense</label>
-          </div>
+          </StyledToggleButton>
 
           <label htmlFor="date">Date</label>
           <input
@@ -84,3 +102,45 @@ export default function TransactionForm({ onAdd }) {
     </>
   );
 }
+
+const StyledToggleButton = styled.div`
+  display: flex;
+  justify-content: space-around;
+  width: 240px;
+  height: 24px;
+  background-color: #f5f5f5;
+  border: 1px solid #141414;
+  border-radius: 30px;
+
+  input {
+    display: none;
+  }
+
+  label {
+    cursor: pointer;
+    border-radius: 30px;
+    width: 120px;
+    text-align: center;
+    color: #141414;
+  }
+
+  label[for="income"] {
+    border-radius: 30px 0 0 30px;
+    border: 0.1px solid #141414;
+    background-color: #e8e9f3;
+  }
+
+  label[for="expense"] {
+    border-radius: 0 30px 30px 0;
+    border: 0.1px solid #141414;
+    background-color: #f8e8e1;
+  }
+
+  input:checked + label {
+    font-weight: 800;
+  }
+
+  input:not(:checked) + label {
+    visitbility: hidden;
+  }
+`;
